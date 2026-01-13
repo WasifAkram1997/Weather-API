4410841402b19507641fedea05f2ffbff5145fda
Gazi Wasif Akram <apexkheliami@gmail.com>
2026-01-12 20:48:24 -0500

    rate limiter integrated

--- src/main.py
+++ src/main.py
@@ -1,13 +1,30 @@
-from fastapi import FastAPI
+import os
+
+from dotenv import load_dotenv
+from fastapi import FastAPI, Depends
+
+from redis.asyncio import Redis
+from fastapi_limiter import FastAPILimiter
+from fastapi_limiter.depends import RateLimiter
+
 from src.services.weather_client import fetch_weather
 from src.exception_handlers import register_exception_handlers
+
+load_dotenv()
 
 app = FastAPI()
 
 register_exception_handlers(app)
+REDIS_URL = os.getenv("REDIS_URL")
 
 
-@app.get("/weather")
+@app.on_event("startup")
+async def startup():
+    redis = Redis.from_url(REDIS_URL, decode_responses=True)
+    await FastAPILimiter.init(redis)
+
+
+@app.get("/weather", dependencies=[Depends(RateLimiter(times=1, seconds=60))])
 async def get_weather(city: str):
     return await fetch_weather(city)
     # return {"message":  f"Hello {city}. It is expected to be {weather_data['days']['description']} today"}
